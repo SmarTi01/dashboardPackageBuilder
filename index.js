0b507ca50681082456c1ed3dae7fdda804badefc
SmarTi01 <tim.smart@lexisnexis.com>
2017-03-31 11:57:10 +0100

    remoave clean path

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var args = process.argv.slice(2);
 // clean(args[0] || './');
 // zip();
 
-exports.clean = function clean(path) {
-    if (fs.existsSync(path + '/content')) fs.unlinkSync(path + '/content');
-    if (fs.existsSync(path + '/dashboard')) fs.unlinkSync(path + '/dashboard');
-    if (fs.existsSync(path + '*.zip')) fs.unlinkSync(path + '*.zip');
-    if (fs.existsSync(path + '/index.html')) fs.unlinkSync(path + 'index.html');
+exports.clean = function clean() {
+    if (fs.existsSync('/content')) fs.unlinkSync(path + '/content');
+    if (fs.existsSync('/dashboard')) fs.unlinkSync(path + '/dashboard');
+    if (fs.existsSync('*.zip')) fs.unlinkSync(path + '*.zip');
+    if (fs.existsSync('index.html')) fs.unlinkSync(path + 'index.html');
 }
 
 exports.zip = function zip() {
