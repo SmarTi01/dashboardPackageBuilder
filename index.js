d44c89e7906d50fc7633dede3910bd81c37de7a8
SmarTi01 <tim.smart@lexisnexis.com>
2017-03-31 11:54:57 +0100

    exposed functions

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ var args = process.argv.slice(2);
 // clean(args[0] || './');
 // zip();
 
-function clean(path) {
+exports.clean = function clean(path) {
     if (fs.existsSync(path + '/content')) fs.unlinkSync(path + '/content');
     if (fs.existsSync(path + '/dashboard')) fs.unlinkSync(path + '/dashboard');
     if (fs.existsSync(path + '*.zip')) fs.unlinkSync(path + '*.zip');
     if (fs.existsSync(path + '/index.html')) fs.unlinkSync(path + 'index.html');
 }
 
-function zip() {
+exports.zip = function zip() {
     zipFolder('dist', 'archive.zip', function(err) {
         if (err) throw err;
     })
