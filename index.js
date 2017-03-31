88699faab2c93c60ba5ab1fcacc468ad88c62da9
SmarTi01 <tim.smart@lexisnexis.com>
2017-03-31 10:45:56 +0100

    added functions£

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var fs = require('fs');
+var admZip = require('adm-zip');
 
-exports.message = function() {
-	console.log('Hello World');
+
+var args = process.argv.slice(2);
+
+
+clean(args[0] || './');
+// zip();
+
+function clean(path) {
+    if (fs.existsSync(path + '/content')) fs.unlinkSync(path + '/content');
+    if (fs.existsSync(path + '/dashboard')) fs.unlinkSync(path + '/dashboard');
+    if (fs.existsSync(path + '*.zip')) fs.unlinkSync(path + '*.zip');
+    if (fs.existsSync(path + '/index.html')) fs.unlinkSync(path + 'index.html');
 }
 
-// function clean(path) {
-//     fs.unlinkSync(path + '/content');
-//     fs.unlinkSync(path + '/dashboard');
-//     fs.unlinkSync(path + '*.zip');
-//     fs.unlinkSync(path + '/index.html');
-// }
+function zip() {
+    var zip = new admZip();
+    zip.addLocalFolder('./dist');
+    zip.writeZip("test.zip");
+}
